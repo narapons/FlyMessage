e967c25b97070f950962fda1921d27f248292642
narapon <43005911+narapons@users.noreply.github.com>
2018-11-29 05:42:53 +0900

    Add files via upload

--- src/FlyMesaage/main.php
+++ src/FlyMesaage/main.php
@@ -1,1 +1,22 @@
+<?php
 
+namespace FlyMessage;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerToggleFlightEvent;
+
+class main extends PluginBase implements Listener
+{
+    public function FlyMessage(PlayerToggleFlightEvent $ev)
+    {
+        $player = $ev->getPlayer();
+        if ($ev->isFlying()) {
+            if(!$player->isOp()){
+            $this->getServer()->getPluginManager()->registerEvents($this, $this); 
+            $name = $player->getName();
+            $this-> getServer()->broadcastMessage("§d【警告】 §f{$name}§dがFlyを使用しました");
+            }
+        }
+    }
+}
